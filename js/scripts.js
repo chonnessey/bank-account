b8cf71aae0979a86bbd89da03617f0ce1542b602
Adrian Camacho <adriancamacho18@gmail.com>
2021-04-06 12:01:58 -0700

    add withdraw and deposit helper functions
    Co-authored-by: Tom/Usarneme <jamestcoop@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 function BankAccount(firstName, lastName, firstDeposit) {
   this.firstName = firstName;
   this.lastName = lastName;
-  this.balance = firstDeposit;
+  this.balance = Number(firstDeposit);
 }
 
 BankAccount.prototype.Deposit = function(amount) {
@@ -21,34 +21,73 @@ BankAccount.prototype.GetUserName = function() {
 }
 
 // UI logic
+// UI Helpers
+function submitHelper(event) {
+  event.preventDefault()
+  const firstName = $("#firstName").val()
+  const lastName = $("#lastName").val()
+  const firstDeposit = $("#firstDeposit").val()
 
+  myBankAccount = new BankAccount(firstName, lastName, firstDeposit)
+  console.log(myBankAccount)
+  // hide the register form
+  $("#register").hide()
+  // put the values into the account page divs
+    // put the name in the welcome div
+  $(".welcome").append(myBankAccount.firstName + " " + myBankAccount.lastName + "'s Account")
+    // put the account balance in the balance div
+  $(".current-balance").text("$" + myBankAccount.balance)
+  // show the account page divs
+  $(".account").show()
+}
+
+function depositHelper() {
+  console.log("deposit button clicked")
+  const depositAmount = Number($("#deposit-input").val())
+
+
+
+  // parseInt("1") => 1
+  // parseInt("123abc") => 123
+  // Number("123abc") => undefined
+  console.log(depositAmount)
+  if (depositAmount === undefined || depositAmount === 0) {
+    return alert("Bad input!")
+  }
+  // if we get past here, we have a good value/input/number
+  myBankAccount.Deposit(depositAmount)
+  console.log(myBankAccount)
+
+  // update the value of the account balance that is displayed
+  $(".current-balance").text("$" + myBankAccount.balance)
+}
+
+function withdrawHelper() {
+  console.log("withdraw button clicked")
+  const withdrawAmount = Number($("#withdraw-input").val())
+
+  // parseInt("1") => 1
+  // parseInt("123abc") => 123
+  // Number("123abc") => undefined
+  console.log(withdrawAmount)
+  if (withdrawAmount === undefined || withdrawAmount === 0) {
+    return alert("Bad input!")
+  }
+  // if we get past here, we have a good value/input/number
+  myBankAccount.Withdraw(withdrawAmount)
+  console.log(myBankAccount)
+
+  // update the value of the account balance that is displayed
+  $(".current-balance").text("$" + myBankAccount.balance)
+}
+
+let myBankAccount
+
+// jQuery doc ready and event listeners
 $(document).ready(function() {
-  $("#register").submit(function(event) {
-    event.preventDefault()
-    const firstName = $("#firstName").val()
-    const lastName = $("#lastName").val()
-    const firstDeposit = $("#firstDeposit").val()
-
-    const myBankAccount = new BankAccount(firstName, lastName, firstDeposit)
-    console.log(myBankAccount)
-    // hide the register form
-    $("#register").hide()
-    // put the values into the account page divs
-      // put the name in the welcome div
-      $(".welcome").append(myBankAccount.firstName + " " + myBankAccount.lastName + "'s Account")
-      // put the account balance in the balance div
-      $(".account-info").append("$" + myBankAccount.balance)
-    // show the account page divs
-    $(".account").show()
-  })
-
-  $("#deposit-button").click(function() {
-    console.log("deposit button clicked")
-  })
-
-  $("#withdraw-button").click(function() {
-    console.log("withdraw button clicked")
-  })
+  $("#register").submit(submitHelper)
+  $("#deposit-button").click(depositHelper)
+  $("#withdraw-button").click(withdrawHelper)
 })
 
 

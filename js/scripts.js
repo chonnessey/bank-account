3f459bb06da737ec809cd6d179294bf20e975cff
Adrian Camacho <adriancamacho18@gmail.com>
2021-04-06 11:19:57 -0700

    add deposit and witdrawls classes with css styles
    Co-authored-by: Tom/Usarneme <jamestcoop@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,11 @@ function BankAccount(firstName, lastName, firstDeposit) {
 }
 
 BankAccount.prototype.Deposit = function(amount) {
-  this.balance = this.balance + amount 
+  this.balance = this.balance + amount
 }
 
 BankAccount.prototype.Withdraw = function(amount) {
-  this.balance = this.balance - amount 
+  this.balance = this.balance - amount
 }
 
 BankAccount.prototype.GetBalance = function() {
@@ -23,7 +23,32 @@ BankAccount.prototype.GetUserName = function() {
 // UI logic
 
 $(document).ready(function() {
-  
+  $("#register").submit(function(event) {
+    event.preventDefault()
+    const firstName = $("#firstName").val()
+    const lastName = $("#lastName").val()
+    const firstDeposit = $("#firstDeposit").val()
+
+    const myBankAccount = new BankAccount(firstName, lastName, firstDeposit)
+    console.log(myBankAccount)
+    // hide the register form
+    $("#register").hide()
+    // put the values into the account page divs
+      // put the name in the welcome div
+      $(".welcome").append(myBankAccount.firstName + " " + myBankAccount.lastName + "'s Account")
+      // put the account balance in the balance div
+      $(".account-info").append("$" + myBankAccount.balance)
+    // show the account page divs
+    $(".account").show()
+  })
+
+  $("#deposit-button").click(function() {
+    console.log("deposit button clicked")
+  })
+
+  $("#withdraw-button").click(function() {
+    console.log("withdraw button clicked")
+  })
 })
 
 
